50e1875fba69be9527b8b67631b808ce8dce60fd
mstiller7 <mstiller@ggc.edu>
2019-10-09 14:43:41 -0400

    Update main.js to operate asynchronously.

--- scripts/main.js
+++ scripts/main.js
@@ -5,39 +5,38 @@ function getCalendarEvents() {
     let year = d.getFullYear();
 
     const userID = '1802612'; // TODO: replace with individual user IDs grabbed from the initial url
-    let html;
 
     // https://ggc.view.usg.edu/d2l/le/calendar/1802612/home/list?day=24&month=9&year=2019
     // day, month, year
-
     const URL = 'https://ggc.view.usg.edu/d2l/le/calendar/' + userID + '/home/list';
     let URLrequest = URL + "?day=" + day + "&month=" + month + "&year=" + year;
-    console.log("Now accessing: ", URLrequest);
 
     fetch(URLrequest)
         .then(function(response) {
             return response.text();
         })
         .then(function(html) {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(html, "text/html");
-            console.log(doc);
+            // HERE is where to call any functions to be called on a successful return of the doc,
+            // due to asynchronous operations.
+            parseCalendarEvents(new DOMParser().parseFromString(html, "text/html"));
         })
         .catch(function(err) {
             console.log("Failed to fetch page: ", err);
         });
 
+}
 
+function parseCalendarEvents(doc) {
     // do things with html var...
     // parse through the calendar element on each iteration of the returned page
     // TODO: then go through the events in the next 6 days upcoming
 
-    const calendar = doc.getElementByClass("d2l-le-calendar-day-events");
+    const calendar = doc.getElementsByClassName("d2l-le-calendar-day-events");
+    console.log("Parsing calendar...");
     for (let halfhour of calendar) {
-        // TODO: Parse the HTML element
         console.log(halfhour);
     }
 }
 
-// TODO: call function on D2L page load
+// TODO: call function on D2L page load. currently it appears to parse whenever a page is loaded on the D2L filetree...
 getCalendarEvents();
