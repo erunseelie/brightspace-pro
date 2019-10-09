56b64c3ec427a1bf043c4b2c55a9998d78be3ffc
mstiller7 <mstiller@ggc.edu>
2019-10-08 22:49:38 -0400

    Fix REST GET request. Now returns HTML document for parsing.

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,42 @@
 function getCalendarEvents() {
-    var d = new Date();
-    var month = d.getMonth();
-    var day = d.getDay();
-    var year = d.getFullYear();
+    let d = new Date();
+    let month = d.getMonth();
+    let day = d.getDay();
+    let year = d.getFullYear();
 
     const userID = '1802612'; // TODO: replace with individual user IDs grabbed from the initial url
-    var html;
+    let html;
 
     // https://ggc.view.usg.edu/d2l/le/calendar/1802612/home/list?day=24&month=9&year=2019
     // day, month, year
 
     const URL = 'https://ggc.view.usg.edu/d2l/le/calendar/' + userID + '/home/list';
+    let URLrequest = URL + "?day=" + day + "&month=" + "&year=" + year + ".html";
+    console.log("Now accessing: ", URLrequest);
 
-    $.ajax({
-        type: 'GET',
-        url: URL,
-        headers: {
-            'day': day,
-            'month': month,
-            'year': year
-        },
-        success: function (result) {
-            html = result;
-        },
-        error: function (result) {
-            console.log('There was an error; ' + result);
-        }
-    });
+    fetch(URLrequest)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(html) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, "text/html");
+            console.log(doc);
+        })
+        .catch(function(err) {
+            console.log("Failed to fetch page: ", err);
+        });
+
 
     // do things with html var...
     // parse through the calendar element on each iteration of the returned page
     // TODO: then go through the events in the next 6 days upcoming
 
-    var calendar = html.getElementByClass("d2l-le-calendar-day-events");
+    const calendar = html.getElementByClass("d2l-le-calendar-day-events");
     for (let halfhour of calendar) {
         // TODO: Parse the HTML element
     }
 }
 
 // TODO: call function on D2L page load
+getCalendarEvents();
