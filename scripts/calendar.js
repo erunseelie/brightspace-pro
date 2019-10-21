5b7986a087009c837d4a64ac33c5b0de72ddc733
mstiller7 <mstiller@ggc.edu>
2019-10-21 14:54:48 -0400

    Add further documentation: - Authorship and designation - Function documentation

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,12 +1,24 @@
+// =================================================================
+// 2019 © Matthew R. Stiller (https://mstiller.net)
+// Designation:
+//      Software Development Capstone w/ Dr. Cengiz Gunay
+//      Fall 2019 @ Georgia Gwinnett College
+// =================================================================
+
 let date = new Date();
 getCalendarEvents(date, 7);
 
-function getCalendarEvents(date, amount) {
+// =================================================================
+
+/**
+ * Returns the sequence of events for the given date, and recursively iterates for the specified number of days.
+ * @param {Date} date The starting date for the sequence.
+ * @param {Number} days Amount of days to iterate.
+ */
+function getCalendarEvents(date, days) {
 
     if (amount == 0) return;
 
-    // https://ggc.view.usg.edu/d2l/le/calendar/1802612/home/list?day=24&month=9&year=2019
-    // day, month, year
     const URL = 'https://ggc.view.usg.edu/d2l/le/calendar/6621';
     let URLrequest = URL + "?day=" + date.getDate() + "&month=" + Number(date.getMonth()+1) + "&year=" + date.getFullYear();
 
@@ -21,7 +33,7 @@ function getCalendarEvents(date, amount) {
             // when the first document has been returned, increase the Date,
             // and recursively call this function.
             date.setDate(date.getDate() + 1);
-            getCalendarEvents(date, amount - 1);
+            getCalendarEvents(date, days - 1);
         })
         .catch(function (err) {
             console.log("Failed to fetch page (" + URLrequest + "): ", err);
@@ -29,6 +41,10 @@ function getCalendarEvents(date, amount) {
 
 }
 
+/**
+ * Parses the D2L calendar events.
+ * @param {HTMLDocument} doc The document to parse.
+ */
 function parseCalendarEvents(doc) {
     const calendar = doc.getElementsByClassName("d2l-le-calendar-day-events");
     for (let halfhour of calendar) {
